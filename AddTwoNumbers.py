e4558bfaba8e061160c689944d4f7f98a7cf678a
Santanu Sarker <sarkersantanu@gmail.com>
2018-02-22 22:08:20 -0500

    Create AddTwoNumbers.py

---
+++ AddTwoNumbers.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        list1 = self._linkedListToNumber(l1)
+        list2 = self._linkedListToNumber(l2)
+        return self._numberToLinkedList(list1+list2)
+        
+    def _linkedListToNumber(self,linked_list):
+        res = []
+        while linked_list:
+            res.append(str(linked_list.val))
+            linked_list = linked_list.next
+        return int("".join(res)[::-1])
+
+    def _numberToLinkedList(self,number):
+        res = head = ListNode(0)
+        for c in str(number)[::-1]:
+            res.next = ListNode(int(c))
+            res = res.next
+        return head.next
