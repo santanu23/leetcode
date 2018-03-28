f6bac28fa7eccfaa6e243fa3d91bff8ab3c6e228
Santanu Sarker <sarkersantanu@gmail.com>
2018-03-28 00:30:21 -0400

    Create InvertBinaryTree.py

---
+++ InvertBinaryTree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        
+        root.left, root.right = root.right, root.left
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
