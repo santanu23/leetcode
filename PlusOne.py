129597f611713c94780de45bac60b89c4ad7508a
Santanu Sarker <sarkersantanu@gmail.com>
2018-06-02 00:00:20 -0400

    Update PlusOne.py

--- PlusOne.py
+++ PlusOne.py
@@ -6,3 +6,11 @@ class Solution:
         """
         res = [int(y) for y in str(int("".join([str(x) for x in digits]))+1)]
         return [0]*(len(digits) - len(res)) + res
+        
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        return [int(c) for c in str(int(reduce(lambda x,y:x+y,[str(d) for d in digits]))+1)]
