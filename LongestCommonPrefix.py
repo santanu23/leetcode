535c561bdfd6fe1fbfef3a7caf4ee50340f13c1f
Santanu Sarker <sarkersantanu@gmail.com>
2018-06-03 23:34:34 -0400

    Create LongestCommonPrefix.py

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        for i, letter_group in enumerate(zip(*strs)):
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
