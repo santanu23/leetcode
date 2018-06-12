6f3ba4f996277699b41d2b9a078d5eac66519222
Santanu Sarker <sarkersantanu@gmail.com>
2018-06-12 00:00:32 -0400

    Create FlippingAnImage.py

---
+++ FlippingAnImage.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[v^1 for v in row[::-1]] for row in A]
