07048ba515d2afeb562c41fd3fa3f2b55aa80925
Santanu Sarker <sarkersantanu@gmail.com>
2018-11-10 10:50:36 -0800

    Update SameTree.py

--- SameTree.py
+++ SameTree.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -5,13 +12,10 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if p is None and q is None:
+        if not p and not q:
             return True
-        elif p is None and q is not None:
+        
+        if p and not q or q and not p or p.val != q.val:
             return False
-        elif q is None and p is not None:
-            return False
-        else:
-            return p.val == q.val\
-                   and self.isSameTree(p.left,q.left) \
-                   and self.isSameTree(p.right,q.right)
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
